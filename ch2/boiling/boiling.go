406f8f58fe514d117b99d4d353716c47348cc1ee
share <share@HPMacBook-Pro.local>
2018-12-10 17:06:36 +0800

    add ch2

--- ch2/boiling/boiling.go
+++ ch2/boiling/boiling.go
@@ -1,1 +1,14 @@
-package boiling
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main(){
+	var f = boilingF
+
+	var c = (f -32) * 5 / 9
+
+	fmt.Printf("boiling pint = %g f or %g c\n", f, c)
+
+}
--- ch2/ftoc/ftoc.go
+++ ch2/ftoc/ftoc.go
@@ -1,1 +1,15 @@
-package ftoc
+package main
+
+import "fmt"
+
+func main(){
+
+	const freezingF , boilingF = 32.0, 212.0
+
+	fmt.Printf("%g F = %g c\n", freezingF, ftoc(freezingF))
+	fmt.Printf("%g F = %g c\n", boilingF, ftoc(boilingF))
+}
+
+func ftoc(f float64) float64 {
+	return (f -32) * 5 / 9
+}
